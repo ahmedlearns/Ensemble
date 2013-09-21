d3b63fbc9c6bc1b193471aaa93fbf228ab5ccf4f
Ahmed Ismail <ahmedismail91@gmail.com>
2013-09-21 12:49:01 -0400

    Update socket code, added example message; ec2 connection testing.

--- client.py
+++ client.py
@@ -1,11 +1,49 @@
-#!/usr/bin/python           # This is client.py file
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+  print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+  sys.exit();
+ 
+print 'Socket Created'
+ 
+host = 'localhost'
+port = 8888
+ 
+try:
+  remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+  #could not resolve
+  print 'Hostname could not be resolved. Exiting'
+  sys.exit()
+     
+print 'Ip address of ' + host + ' is ' + remote_ip
+ 
+#Connect to remote server
+s.connect((remote_ip , port))
+ 
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
 
-import socket               # Import socket module
+#Send some data to remote server
+message = "GET / HTTP/1.1\r\n\r\n"
+ 
+try :
+    #Set the whole string
+    s.sendall(message)
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+ 
+print 'Message send successfully'
+ 
+#Now receive data
+reply = s.recv(4096)
+ 
+print reply
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-
-s.connect((host, port))
-print s.recv(1024)
-s.close                     # Close the socket when done
+s.close()
--- server.py
+++ server.py
@@ -1,15 +1,35 @@
-#!/usr/bin/python           # This is server.py file
-
-import socket               # Import socket module
-
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+import socket
+import sys
+ 
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+ 
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+ 
+s.listen(10)
+print 'Socket now listening'
+ 
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+     
+    data = conn.recv(1024)
+    reply = 'OK...' + data
+    if not data: 
+        break
+     
+    conn.sendall(reply)
+ 
+conn.close()
+s.close()
