e56a2ebec464e79763631ca99ecd5ff6014525fc
Ahmed Ismail <ahmedismail91@gmail.com>
2013-09-21 17:41:27 -0400

    Added extract_feature.py

--- extract_features.py
+++ extract_features.py
@@ -17,57 +17,66 @@ def extract_features(img):
   key_points = detector.detect(img)
   key_points, descr = descriptor.compute(img, key_points)
 
-  # print len(key_points)
-  # print descr.shape
-  # print type(float(descr[0][0]))
-
-  #######################################################################
-  # serializable = []
-  # for i = range(0 to len(key_points)):
-  #   serializable.append((kp.pt, kp.size, kp.angle, kp.response, kp.octave, kp.class_id, float(descr[i]) ))
-  #######################################################################
-
-
   kp_serializable = []
   for kp in key_points:
     kp_serializable.append((kp.pt, kp.size, kp.angle, kp.response, kp.octave, kp.class_id))
 
-  f = open("kp_dump.pkl", 'wb')
+  f = open("kp_dump.pkl", 'w')
   pickle.dump(kp_serializable, f)
   f.close()
-  f = open("des_dump.pkl", 'wb')
+  f = open("des_dump.pkl", 'w')
   pickle.dump(descr, f)
   f.close()
 
-  kp_serializable = pickle.load(open("kp_dump.pkl", 'r'))
-  descr = pickle.load(open("des_dump.pkl", 'r'))
 
-  key_points = []
-  for kp in kp_serializable:
-    key_points.append(cv2.KeyPoint(x=kp[0][0],y=kp[0][1], _size=kp[1], _angle=kp[2], 
-                            _response=kp[3], _octave=kp[4], _class_id=kp[5]))
-  
+  #### DESERIALIZING - FOR RETRIEVING INFO #########
+  # kp_serializable = pickle.load(open("kp_dump.pkl", 'r'))
+  # descr = pickle.load(open("des_dump.pkl", 'r'))
 
-  # descr_serializable = []
-  # for i in xrange(len(descr)):
-  #   descr_serializable.append([0 for j in xrange(len(descr[i]))])
-
-  # # print descr_serializable
-
-  # for d in range(len(descr)):
-  #   descr_serializable.append([float(x) for x in descr[d]])
-    # for x in descr[d]:
-    #   descr_serializable[d].append(float(x))
-
-  # print descr_serializable
-  # print key_points[0].angle
-  # x = yaml.dump({'key_points': kp_serializable}) #, 'descriptors': descr})
-  # print yaml.load(x)
+  # key_points = []
+  # for kp in kp_serializable:
+  #   key_points.append(cv2.KeyPoint(x=kp[0][0],y=kp[0][1], _size=kp[1], _angle=kp[2], 
+  #                           _response=kp[3], _octave=kp[4], _class_id=kp[5]))
 
 
-  # print type(descr[0][0])
-  # x =yaml.dump({'test': descr_serializable})
-  # print yaml.load(x);
-  # print x
+def get_exif(fn):
+    ret = {}
+    i = Image.open(fn)
+    info = i._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        if decoded in ["Make", "DateTime", "Model"]:
+      ret[decoded] = value
+  elif decoded == "GPSInfo":
+            if len(value) > 2:
+                longitude = value[2]
+              print longitude
+                longitude = (float)(longitude[0][0]*1.0 + longitude[1][0]/60.0 + longitude[2][0]/3600.)
+          if value[1] == 'S':
+                    longitude *= -1
+               
+                latitude = value[4]
+          latitude = (float)(latitude[0][0]*1.0 + latitude[1][0]/60.0 + latitude[2][0]/3600.0)
+          if value[3] == 'W':
+              latitude *= -1
+                ret[decoded] = [longitude, latitude]
+            else:
+                 ret[decoded] = None
 
-extract_features(cv2.imread(sys.argv[1]))
+    return ret
+
+
+#prevents websites thinking that this code is from a spam/robot/hacker, even though it is...
+class MyOpener(FancyURLopener):
+    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+
+if len(sys.argv) == 2:
+    myopener = MyOpener()
+    myopener.retrieve(sys.argv[1], "downloaded_image.jpg")
+    extract_features(cv2.imread("downloaded_image.jpg"))
+    exif = get_exif("downloaded_image.jpg")
+    #store into rails db?!
+    print exif
+    os.remove("downloaded_image.jpg")
+else:
+    sys.exit("Incorrect number of arguments")
