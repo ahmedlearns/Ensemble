f4af32bbeadf078d20f20d656853af7540997e8f
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-09-21 09:53:05 -0400

    initial commit for website

---
+++ ensemble/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ensemble::Application.config.secret_token = '8f17671b1e8318311692349304ace177ec6a49a375ee4068f7b5c5e6a0f76c26be0b1de7a3a1e60d8502835ac9553fbeca7e9b2856872dd4f0be07eee425f2a6'
