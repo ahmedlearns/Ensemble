6093c1cf17ad299e0e348854ce7f0cc1ae2dd3d0
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-09-22 00:28:22 -0400

    reverting chris's commit

--- ensemble/server/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '2dc9a236441b6552032d92b4085b1c857f6de0bc9136ef6956476f41f99fc4daa13c745c174449611c5432f4194050a35de2f849a52df032e283ae53179161c3';
--- ensemble/vendor/cache/ruby/1.8/gems/less-rails-2.3.2/lib/less/rails/version.rb
+++
@@ -1,5 +0,0 @@
-module Less
-  module Rails
-    VERSION = "2.3.2"
-  end
-end
