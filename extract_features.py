2a45bd06c139b085892686359b97e226414ab060
Ahmed Ismail <ahmedismail91@gmail.com>
2013-09-21 22:54:58 -0400

    Import os

--- extract_features.py
+++ extract_features.py
@@ -6,7 +6,7 @@ import pickle
 import cv2
 import numpy as np
 import itertools
-import sys
+import sys, os
 import yaml
 
 
@@ -45,26 +45,26 @@ def get_exif(fn):
         i = Image.open(fn)
         info = i._getexif()
     except:
+        print "I'm here"
         sys.exit()
     for tag, value in info.items():
         decoded = TAGS.get(tag, tag)
         if decoded in ["Make", "DateTime", "Model"]:
-      ret[decoded] = value
-  elif decoded == "GPSInfo":
+            ret[decoded] = value
+        elif decoded == "GPSInfo":
             if len(value) > 2:
                 longitude = value[2]
-              print longitude
+                # print longitude
                 longitude = (float)(longitude[0][0]*1.0 + longitude[1][0]/60.0 + longitude[2][0]/3600.)
-          if value[1] == 'S':
+                if value[1] == 'S':
                     longitude *= -1
-               
                 latitude = value[4]
-          latitude = (float)(latitude[0][0]*1.0 + latitude[1][0]/60.0 + latitude[2][0]/3600.0)
-          if value[3] == 'W':
-              latitude *= -1
+                latitude = (float)(latitude[0][0]*1.0 + latitude[1][0]/60.0 + latitude[2][0]/3600.0)
+                if value[3] == 'W':
+                    latitude *= -1
                 ret[decoded] = [longitude, latitude]
             else:
-                 ret[decoded] = None
+                ret[decoded] = None
 
     return ret
 
