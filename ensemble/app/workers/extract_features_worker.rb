857be559eb0339af51075ae6731190e50f832670
Sarvagya Vaish <sarvagya.vaish7@gmail.com>
2013-09-22 01:26:58 -0400

    check in worker

--- ensemble/app/workers/extract_features_worker.rb
+++ ensemble/app/workers/extract_features_worker.rb
@@ -2,23 +2,28 @@ class ExtractFeaturesWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+
   def perform(id)
     image = Image.find(id)
     puts "Doing hard work on #{image.id}"
 
     port = 8888
     host = "ec2-54-224-83-142.compute-1.amazonaws.com"
-    socket = TCPSocket.open(host,port)
+    begin
+      socket = TCPSocket.open(host,port)
+      path = image.path
+      string_to_send = {script: 0, url: path}
+      socket.puts(string_to_send.to_json)
+      puts string_to_send.to_json
+      socket.close()
+    rescue
+      puts "Could not connect to server"
 
-    path = image.path
-    string_to_send = {script: 0, url: path}
-    socket.puts(string_to_send.to_json)
-    puts string_to_send.to_json
+    end
 
     #uri = URI.parse("ec2-54-224-83-142.compute-1.amazonaws.com")
     #request = Net::HTTP.post_form(uri, lang: "aaa", code: "asas")
 
-    socket.close()
 
     #    @snippet.update_attribute(:highlighted_code, request.body)
   end
